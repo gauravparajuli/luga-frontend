0e26c8dcdb28eb4d4f0c9d0d2c2de522e041cb62
gaurav parajuli <gparajuli.1997@gmail.com>
2023-02-10 19:10:18 +0545

    added product details page

--- src/screens/Product.jsx
+++ src/screens/Product.jsx
@@ -1,33 +1,39 @@
 import React from 'react'
+import Layout from '../components/Layout'
+
+import Button from '../components/Button'
 
 const Product = () => {
     return (
         <>
-            <Navbar />
-            <div className='container mx-auto flex justify-between'>
-                {/* image container */}
-                <div>
-                    <img
-                        src='https://i.ibb.co/S6qMxwr/jean.jpg'
-                        className='h-[90%]'
-                        alt=''
-                    />
+            <Layout>
+                <h1 className='mb-2 mt-8 text-center'>Denim Jeans</h1>
+                <div className='flex flex-col md:flex-row justify-between'>
+                    {/* image container */}
+                    <div>
+                        <img
+                            src='https://i.ibb.co/S6qMxwr/jean.jpg'
+                            className='h-[90%]'
+                            alt=''
+                        />
+                    </div>
+                    {/* product info container  */}
+                    <div className='flex flex-col gap-4 text-lg'>
+                        <p className='text-lg'>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit. Donec venenatis, dolor in finibus malesuada,
+                            lectus ipsum porta nunc, at iaculis arcu nisi sed
+                            mauris. Nulla fermentum vestibulum ex, eget
+                            tristique tortor pretium ut. Curabitur elit justo,
+                            consequat id condimentum ac, volutpat ornare.
+                        </p>
+                        <p className='font-bold'>$ 20</p>
+                        <div className='md:w-[200px] my-4'>
+                            <Button>ADD TO CART</Button>
+                        </div>
+                    </div>
                 </div>
-                {/* product info container  */}
-                <div>
-                    <h3 className='text-3xl'>Denim Jumpsuit</h3>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Donec venenatis, dolor in finibus malesuada, lectus
-                        ipsum porta nunc, at iaculis arcu nisi sed mauris. Nulla
-                        fermentum vestibulum ex, eget tristique tortor pretium
-                        ut. Curabitur elit justo, consequat id condimentum ac,
-                        volutpat ornare.
-                    </p>
-                    <p>$ 20</p>
-                </div>
-            </div>
-            <Footer />
+            </Layout>
         </>
     )
 }
