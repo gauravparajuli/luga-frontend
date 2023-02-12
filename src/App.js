3ab956db5173e89d00eb3e635b06875d2bea3885
gaurav parajuli <gparajuli.1997@gmail.com>
2023-02-12 06:57:44 +0545

    fixed routing for login and signup and added links to category card

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import Home from './screens/Home'
 import ProductList from './screens/ProductList'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, Navigate } from 'react-router-dom'
 import Pay from './screens/Pay'
 import PaymentSuccessful from './screens/PaymentSuccessful'
 import Product from './screens/Product'
@@ -11,14 +11,21 @@ import Signup from './screens/Signup'
 import Cart from './screens/Cart'
 
 const App = () => {
+    const user = true
     return (
         <Routes>
             <Route path='/' element={<Home />} />
-            <Route path='/login' element={<Login />} />
+            <Route
+                path='/login'
+                element={user ? <Navigate to='/' /> : <Login />}
+            />
             <Route path='/cart' element={<Cart />} />
-            <Route path='/signup' element={<Signup />} />
+            <Route
+                path='/signup'
+                element={user ? <Navigate to='/' /> : <Signup />}
+            />
             <Route path='/product/:id' element={<Product />} />
-            <Route path='/shop' element={<ProductList />} />
+            <Route path='/products/:category' element={<ProductList />} />
             <Route path='/pay' element={<Pay />} />
             <Route path='/success' element={<PaymentSuccessful />} />
         </Routes>
--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom'
+
 const CategoryItem = ({ category }) => {
-    const { img, title } = category
+    const { img, title, cat } = category
+    console.log(`/products/${cat}`)
     return (
         <div className='relative group cursor-pointer h-[400px] w-[100vw]'>
-            <img src={img} className='h-full w-full object-cover' />
-            <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-3 flex flex-col justify-center text-white border w-1/3 h-1/3 bg-gray-900 opacity-50 group-hover:opacity-100 text-center tranisition-all'>
-                {title}
-                <br />
-                SHOP NOW
-            </div>
+            <Link to={`/products/${cat}`}>
+                <img src={img} className='h-full w-full object-cover' />
+                <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-3 flex flex-col justify-center text-white border w-1/3 h-1/3 bg-gray-900 opacity-50 group-hover:opacity-100 text-center tranisition-all'>
+                    {title}
+                    <br />
+                    SHOP NOW
+                </div>
+            </Link>
         </div>
     )
 }
